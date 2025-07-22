0a7ee8774ca4b34a71cb854fde38023cc8960153
SarthakAg04 <sarthak.ag2711@gmail.com>
2025-07-22 04:47:15 +0000

    container config done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3+3) //we are using node js not browser
+
+console.log(
+    3+
+    3) // code redability should be high
+
+console.log("Hitesh")
+
+let name  = "sarthak"
+let age =18
+let isLogged = false
+let state;
+
+
+// number = 2^53 
+//bigint
+// staring = ""
+
+// boolean = true/false
+// null = standalone value
+// undefined = 
+// symbol  = unique
+
+// object
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
+
